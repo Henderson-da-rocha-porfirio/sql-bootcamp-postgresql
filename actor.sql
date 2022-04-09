3ebbfe5709aa5f80397f7ee4af1ee52673258197
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 17:28:31 -0300

    Create actor.sql

---
+++ actor.sql
@@ -0,0 +1,11 @@
+-- Select table
+SELECT * FROM actor
+
+-- Select uma coluna
+SELECT actor.first_name, actor.last_name FROM actor
+
+-- Select duas colunas
+SELECT actor.first_name, actor.last_name FROM actor
+
+-- Select duas colunas com coluna invertida
+SELECT  actor.last_name,actor.first_name FROM actor
