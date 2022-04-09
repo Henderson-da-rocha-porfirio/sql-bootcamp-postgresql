fc3a7f73d9e1d2ed5a78f8e9b1f5010fe735adb4
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 19:56:44 -0300

    Update payment.sql

--- payment.sql
+++ payment.sql
@@ -1,23 +1,29 @@
 -- PAYMENT
 SELECT * FROM payment
+
 -- exemplo
 SELECT * FROM payment
 ORDER BY payment.payment_date
+
 -- exemplo 2
 SELECT * FROM payment
 ORDER BY payment.payment_date ASC
+
 -- exemplo 3
 SELECT * FROM payment
 ORDER BY payment.payment_date DESC
+
 -- exemplo 3
 SELECT * FROM payment
 ORDER BY payment.payment_date DESC
 LIMIT 5
+
 -- exemplo 4
 SELECT * FROM payment
 WHERE payment.amount != 0.00
 ORDER BY payment.payment_date DESC
 LIMIT 5
+
 -- exemplo 4
 SELECT * FROM payment
 LIMIT 1
