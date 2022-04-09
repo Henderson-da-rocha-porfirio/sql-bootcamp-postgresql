c322a7d782fd51b5b5eaa383cf457add62c84efc
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 18:02:10 -0300

    Update film.sql

--- film.sql
+++ film.sql
@@ -6,3 +6,7 @@ SELECT DISTINCT film.release_year FROM film
 SELECT DISTINCT (film.release_year) FROM film
 
 SELECT DISTINCT (film.rental_rate) FROM film
+
+-- PAYMENT
+-- verificando numeros distintos
+SELECT DISTINCT payment.amount FROM payment
