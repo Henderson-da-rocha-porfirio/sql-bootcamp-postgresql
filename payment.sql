a4c46526f4c603d163dad1243d21afe2edb00cc4
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 19:55:56 -0300

    Update payment.sql

--- payment.sql
+++ payment.sql
@@ -1,25 +1,40 @@
 -- PAYMENT
 SELECT * FROM payment
+
 -- CONTAR AS ROWS NA TABELA
 SELECT COUNT (*) FROM payment
+
 -- CONTAR AS ROWS COM UMA COLUNA ESPECÍFICA NA TABELA
 SELECT COUNT (payment.staff_id) FROM payment
+
 -- verificando numeros distintos
 SELECT DISTINCT payment.amount FROM payment
+
 -- verificando a quantidade de numeros distintos
 SELECT COUNT (DISTINCT payment.amount) FROM payment
 SELECT COUNT (DISTINCT (payment.amount)) FROM payment
+
 -- exemplo com COUNT
 SELECT COUNT (film.title) FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
 AND film.rating = 'R'
+
 -- exemplo com COUNT 2
 SELECT COUNT (*) FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
 AND film.rating = 'R'
+
 -- exemplo com COUNT 3
 SELECT COUNT (*) FROM film
 WHERE film.rating = 'R' OR film.rating = 'PG-13'
+
+-- exemplo
+SELECT COUNT(*) FROM payment
+WHERE payment.amount BETWEEN 8 AND 9
+
+-- exemplo
+SELECT COUNT(*) FROM payment
+WHERE payment.amount NOT BETWEEN 8 AND 9
 
 -- FILM COUNT
 SELECT COUNT(film.title) FROM film
