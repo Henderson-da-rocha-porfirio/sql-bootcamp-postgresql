a366f3c988acde284603c0cc310b446e7ced84bf
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 18:05:07 -0300

    Update film.sql

--- film.sql
+++ film.sql
@@ -10,3 +10,7 @@ SELECT DISTINCT (film.rental_rate) FROM film
 -- PAYMENT
 -- verificando numeros distintos
 SELECT DISTINCT payment.amount FROM payment
+
+-- verificando a quantidade de numeros distintos com count
+SELECT COUNT (DISTINCT payment.amount) FROM payment
+SELECT COUNT (DISTINCT (payment.amount)) FROM payment
