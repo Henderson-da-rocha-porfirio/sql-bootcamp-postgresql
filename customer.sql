e1ec4dda1964b91f3330669907e9bcac699de0a0
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 19:58:09 -0300

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -1,38 +1,46 @@
 -- CUSTOMER WHERE
 SELECT * FROM customer
+
 -- Selecionar os dados de um consumidor pelo primeiro nome dele 
 SELECT * FROM customer
 WHERE customer.first_name = 'Jared'
 
 
-
 -- FILM WHERE
 SELECT * FROM film
+
 -- exemplo 2
 SELECT * FROM film
 WHERE film.rental_rate > 4
+
 -- exemplo 3
 SELECT * FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
+
 -- exemplo 4
 SELECT * FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
 AND film.rating = 'R'
+
 -- exemplo 5
 SELECT film.title FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
 AND film.rating = 'R'
+
 -- exemplo com COUNT
 SELECT COUNT (film.title) FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
 AND film.rating = 'R'
+
 -- exemplo com COUNT 2
 SELECT COUNT (*) FROM film
 WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
 AND film.rating = 'R'
+
 -- exemplo com COUNT 3
 SELECT COUNT (*) FROM film
 WHERE film.rating = 'R' OR film.rating = 'PG-13'
+
 -- exemplo 6
 SELECT * FROM film
 WHERE film.rating != 'R'
@@ -41,6 +49,7 @@ WHERE film.rating != 'R'
 
 -- PAYMENT WHERE
 SELECT * FROM payment
+
 -- exemplo
 SELECT * FROM payment
 WHERE payment.amount != 0.00
