54d22b02370915004e6af4e9937e3f1df095188a
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 18:26:41 -0300

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -1,11 +1,35 @@
--- PAYMENT
-SELECT * FROM payment
--- CONTAR AS ROWS NA TABELA
-SELECT COUNT (*) FROM payment
--- CONTAR AS ROWS COM UMA COLUNA ESPECÍFICA NA TABELA
-SELECT COUNT (payment.staff_id) FROM payment
--- verificando numeros distintos
-SELECT DISTINCT payment.amount FROM payment
--- verificando a quantidade de numeros distintos
-SELECT COUNT (DISTINCT payment.amount) FROM payment
-SELECT COUNT (DISTINCT (payment.amount)) FROM payment
+-- CUSTOMER
+SELECT * FROM customer
+-- Selecionar os dados de um consumidor pelo primeiro nome dele 
+SELECT * FROM customer
+WHERE customer.first_name = 'Jared'
+-- FILM
+SELECT * FROM film
+-- exemplo 2
+SELECT * FROM film
+WHERE film.rental_rate > 4
+-- exemplo 3
+SELECT * FROM film
+WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
+-- exemplo 4
+SELECT * FROM film
+WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
+AND film.rating = 'R'
+-- exemplo 5
+SELECT film.title FROM film
+WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
+AND film.rating = 'R'
+-- exemplo com COUNT
+SELECT COUNT (film.title) FROM film
+WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
+AND film.rating = 'R'
+-- exemplo com COUNT 2
+SELECT COUNT (*) FROM film
+WHERE film.rental_rate > 4 AND film.replacement_cost >= 19.99
+AND film.rating = 'R'
+-- exemplo com COUNT 3
+SELECT COUNT (*) FROM film
+WHERE film.rating = 'R' OR film.rating = 'PG-13'
+-- exemplo 6
+SELECT * FROM film
+WHERE film.rating != 'R'
