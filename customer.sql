906831a1440cfce0623b9d04e426774e84f79d73
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-09 19:54:41 -0300

    Update customer.sql

--- customer.sql
+++ customer.sql
@@ -51,3 +51,24 @@ LIMIT 5
 -- film where
 SELECT COUNT(film.title) FROM film
 WHERE film.length <= 50
+
+
+-- PAYMENT WHERE
+SELECT * FROM payment
+WHERE payment.amount BETWEEN 8 AND 9
+
+-- exemplo
+SELECT COUNT(*) FROM payment
+WHERE payment.amount BETWEEN 8 AND 9
+
+-- exemplo
+SELECT COUNT(*) FROM payment
+WHERE payment.amount NOT BETWEEN 8 AND 9
+
+-- exemplo
+SELECT * FROM payment
+WHERE payment.payment_date BETWEEN '2007-02-01' AND '2007-02-15'
+
+-- """ ATENÇÃO """  - Não pega nada do database porque tem que ser sempre uma data posterior
+SELECT * FROM payment
+WHERE payment.payment_date BETWEEN '2007-02-01' AND '2007-02-14'
