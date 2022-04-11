a32b168fcd3e68b7ba988c05eb267dfa4d8b8952
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-11 16:26:02 -0300

    Update film.sql

--- film.sql
+++ film.sql
@@ -1,6 +1,2 @@
--- FILM - saber A MÉDIA
-SELECT AVG(film.replacement_cost) FROM film
-
--- FILM - arredondar o valor da MÉDIA COM APENAS O VALOR E CENTAVOS, POR ISSO o 2 alí, como exemplo:
-SELECT ROUND (AVG(film.replacement_cost),2) FROM film
-
+-- FILM - SOMAR O VALOR
+SELECT SUM(film.replacement_cost) FROM film
