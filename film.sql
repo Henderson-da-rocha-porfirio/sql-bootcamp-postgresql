ee46c0593b9299d3366c5b031f204ce22b262001
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-11 16:24:18 -0300

    Update film.sql

--- film.sql
+++ film.sql
@@ -1,3 +1,5 @@
-SELECT MIN(film.replacement_cost) FROM film
+-- FILM - saber A MÉDIA
+SELECT AVG(film.replacement_cost) FROM film
 
-SELECT MAX(film.replacement_cost),MIN(film.replacement_cost) FROM film
+-- FILM - arredondar o valor da MÉDIA COM APENAS O VALOR E CENTAVOS, POR ISSO o 2 alí, como exemplo:
+SELECT ROUND (AVG(film.replacement_cost),2) FROM film
