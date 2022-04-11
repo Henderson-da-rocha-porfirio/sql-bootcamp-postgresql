786d87a3371a8f65f4430a10caf0ce38fec19c84
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-11 18:32:30 -0300

    Update payment.sql

--- payment.sql
+++ payment.sql
@@ -1,36 +1,29 @@
 -- PAYMENT
 SELECT * FROM payment
 
--- PAYMENT
-SELECT payment.customer_id,SUM(payment.amount) FROM payment
-WHERE payment.customer_id NOT IN (184,87,477)
-GROUP BY payment.customer_id
+-- PAYMENT - quantas transações ocorrem - usando um alias num_transactions
+SELECT COUNT(payment.amount) AS num_transactions
+FROM payment
 
--- PAYMENT
-SELECT payment.customer_id,SUM(payment.amount) FROM payment
+-- PAYMENT - quantas transações ocorrem - usando um alias num_transactions
+SELECT COUNT(*) AS num_transactions
+FROM payment
+
+-- CUSTOMER
+SELECT customer_id, SUM(amount)
+FROM payment
+GROUP BY customer_id
+
+SELECT customer_id, SUM(amount) AS total_spent
+FROM payment
+GROUP BY customer_id
+
+SELECT payment.customer_id, SUM(amount)
+FROM payment
 GROUP BY payment.customer_id
 HAVING SUM(payment.amount) > 100
 
-
--- CUSTOMER
-SELECT * FROM customer
-
-SELECT customer.store_id,COUNT(customer.customer_id) FROM customer
-GROUP BY customer.store_id
-
-SELECT customer.store_id,COUNT(*) FROM customer
-GROUP BY customer.store_id
-
-SELECT customer.store_id,COUNT(*) FROM customer
-GROUP BY customer.store_id
-HAVING COUNT(*) > 300
-
-SELECT customer.store_id,COUNT(customer.customer_id) FROM customer
-GROUP BY customer.store_id
-HAVING COUNT(customer.customer_id) > 300
-
-SELECT customer_id, SUM(amount)
+SELECT payment.customer_id, SUM(amount) AS total_spent
 FROM payment
-WHERE staff_id = 2
-GROUP BY customer_id
-HAVING SUM(amount) > 100
+GROUP BY payment.customer_id
+HAVING SUM(payment.amount) > 100
