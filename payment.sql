c233959dd41aa2c8e6a2f5ec792f6e402589fc87
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-11 18:04:40 -0300

    Update payment.sql

--- payment.sql
+++ payment.sql
@@ -76,3 +76,38 @@ SELECT payment.customer_id,SUM(payment.amount) FROM payment
 GROUP BY payment.customer_id
 ORDER BY SUM(payment.amount) DESC
 LIMIT 5
+
+
+-- PAYMENT
+SELECT payment.customer_id,SUM(payment.amount) FROM payment
+WHERE payment.customer_id NOT IN (184,87,477)
+GROUP BY payment.customer_id
+
+-- PAYMENT
+SELECT payment.customer_id,SUM(payment.amount) FROM payment
+GROUP BY payment.customer_id
+HAVING SUM(payment.amount) > 100
+
+
+-- CUSTOMER
+SELECT * FROM customer
+
+SELECT customer.store_id,COUNT(customer.customer_id) FROM customer
+GROUP BY customer.store_id
+
+SELECT customer.store_id,COUNT(*) FROM customer
+GROUP BY customer.store_id
+
+SELECT customer.store_id,COUNT(*) FROM customer
+GROUP BY customer.store_id
+HAVING COUNT(*) > 300
+
+SELECT customer.store_id,COUNT(customer.customer_id) FROM customer
+GROUP BY customer.store_id
+HAVING COUNT(customer.customer_id) > 300
+
+SELECT customer_id, SUM(amount)
+FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) > 100
