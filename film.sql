7b0456c9e88a937c5a0d353e8eb49e10319331b3
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-12 22:43:28 -0300

    Update film.sql

--- film.sql
+++ film.sql
@@ -1,3 +1,15 @@
-SELECT film.film_id FROM film
-UNION
-SELECT inventory.film_id FROM inventory
+SELECT * FROM film
+
+SELECT film.rental_rate/replacement_cost FROM film
+
+SELECT ROUND(film.rental_rate/replacement_cost,2) FROM film
+
+SELECT ROUND(film.rental_rate/replacement_cost,2)*100 FROM film
+
+SELECT ROUND(film.rental_rate/replacement_cost,4)*100 FROM film
+
+SELECT ROUND(film.rental_rate/replacement_cost,4)*100 AS custo_percentual
+FROM film
+
+SELECT 0.1 * film.replacement_cost AS deposito
+FROM film
