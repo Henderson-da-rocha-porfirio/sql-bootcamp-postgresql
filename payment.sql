cd96a4b3ac37ef0470b364858ce482a55877dbf6
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-12 23:43:54 -0300

    Update payment.sql

--- payment.sql
+++ payment.sql
@@ -2,35 +2,66 @@
 SELECT * FROM payment
 
 -- PAYMENT
-SELECT payment.customer_id,SUM(payment.amount) FROM payment
-WHERE payment.customer_id NOT IN (184,87,477)
-GROUP BY payment.customer_id
+-- PAYMENT (table A)
+SELECT * FROM payment
 
--- PAYMENT
-SELECT payment.customer_id,SUM(payment.amount) FROM payment
-GROUP BY payment.customer_id
-HAVING SUM(payment.amount) > 100
-
-
--- CUSTOMER
+-- CUSTOMER (table B)
 SELECT * FROM customer
 
-SELECT customer.store_id,COUNT(customer.customer_id) FROM customer
-GROUP BY customer.store_id
+-- INNER JOIN
+SELECT * FROM payment
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id
 
-SELECT customer.store_id,COUNT(*) FROM customer
-GROUP BY customer.store_id
-
-SELECT customer.store_id,COUNT(*) FROM customer
-GROUP BY customer.store_id
-HAVING COUNT(*) > 300
-
-SELECT customer.store_id,COUNT(customer.customer_id) FROM customer
-GROUP BY customer.store_id
-HAVING COUNT(customer.customer_id) > 300
-
-SELECT customer_id, SUM(amount)
+-- INNER JOIN - síntaxe geral
+SELECT payment.payment_id,payment.customer_id,customer.first_name
 FROM payment
-WHERE staff_id = 2
-GROUP BY customer_id
-HAVING SUM(amount) > 100
+INNER JOIN customer
+ON payment.customer_id = customer.customer_id
+
+-- INNER JOIN - síntaxe geral 2 - a ordem não importa
+SELECT payment.payment_id,payment.customer_id,customer.first_name
+FROM customer
+INNER JOIN payment
+ON payment.customer_id = customer.customer_id
+
+
+
+SELECT * FROM rental
+
+SELECT * FROM inventory
+
+SELECT * FROM rental
+WHERE rental.return_date BETWEEN '2005-05-29' AND '2005-05-30'
+
+SELECT inventory.film_id
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE rental.return_date BETWEEN '2005-05-29' AND '2005-05-30'
+
+
+SELECT film.film_id,film.title
+FROM film
+WHERE film.film_id IN
+(SELECT inventory.film_id
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE rental.return_date BETWEEN '2005-05-29' AND '2005-05-30')
+
+SELECT film.film_id,film.title
+FROM film
+WHERE film.film_id IN
+(SELECT inventory.film_id
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE rental.return_date BETWEEN '2005-05-29' AND '2005-05-30')
+ORDER BY film.title
+
+SELECT film.film_id,film.title
+FROM film
+WHERE film.film_id IN
+(SELECT inventory.film_id
+FROM rental
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+WHERE rental.return_date BETWEEN '2005-05-29' AND '2005-05-30')
+ORDER BY film.film_id
