7a90003ed91e2800cc0321184f7f9514499a95b9
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-14 18:15:51 -0300

    Update conta.sql

--- conta.sql
+++ conta.sql
@@ -1,3 +1,8 @@
 UPDATE conta
 SET ultimo_login = CURRENT_TIMESTAMP
     WHERE ultimo_login IS NULL;
+
+
+-- Reset everything without WHERE condition
+UPDATE conta
+SET ultimo_login = CURRENT_TIMESTAMP
