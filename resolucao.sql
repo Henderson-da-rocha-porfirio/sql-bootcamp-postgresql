561141bf82afc22c7a8c78a1af1e4c278b6318fd
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-14 17:27:31 -0300

    Update resolucao.sql

--- resolucao.sql
+++ resolucao.sql
@@ -1,81 +1,19 @@
--- SOLUTIONS TO ASSESSMENT TEST 2
+CREATE TABLE conta(
+usuario_id SERIAL PRIMARY KEY,
+nomeusuario VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(50) NOT NULL,
+email VARCHAR(250) UNIQUE NOT NULL,
+criado_em TIMESTAMP NOT NULL,
+ultimo_login TIMESTAMP
+)
 
--- 1. Solucao:
+CREATE TABLE trabalho(
+trabalho_id SERIAL PRIMARY KEY,
+trabalho_nome VARCHAR(200) UNIQUE NOT NULL,
+)
 
-SELECT * FROM cd.facilities;
-
--- 2. Solucao:
-
-SELECT name, membercost FROM cd.facilities;
-
--- 3. Solucao:
-
- SELECT * FROM cd.facilities WHERE membercost > 0;
-
--- 4. Solucao:
-
- SELECT facid, name, membercost, monthlymaintenance
- FROM cd.facilities
- WHERE membercost > 0 AND
- (membercost < monthlymaintenance/50.0);
- 
- -- 5. Solucao:
-
- SELECT * FROM cd.facilities WHERE name LIKE '%Tennis%';
-
--- 6. Solucao:
-
-SELECT * FROM cd.facilities WHERE facid IN (1,5);
-
- 
- -- 7. Solucao:
-
-SELECT memid, surname, firstname, joindate 
-FROM cd.members WHERE joindate >= '2012-09-01';
-
-
--- 8. Solucao:
-SELECT DISTINCT surname FROM cd.members
-ORDER BY  surname LIMIT 10;
- 
- -- 9. Solucao:
-
- SELECT MAX(joindate) AS latest_signup FROM cd.members;
-
--- 10. Solucao:
-
-SELECT COUNT(*) FROM cd.facilities WHERE guestcost >= 10;
-
-  
- -- 11. Solucao:
-
-SELECT facid, sum(slots) AS "Total Slots" FROM cd.bookings WHERE starttime >= '2012-09-01' AND starttime < '2012-10-01' GROUP BY facid ORDER BY SUM(slots);
-
-
--- 12. Solucao:
- SELECT facid, sum(slots) AS total_slots FROM cd.bookings GROUP BY facid HAVING SUM(slots) > 1000 ORDER BY facid
- 
- -- 13. Solucao:
-
-SELECT cd.bookings.starttime AS start, cd.facilities.name 
-AS name 
-FROM cd.facilities 
-INNER JOIN cd.bookings
-ON cd.facilities.facid = cd.bookings.facid 
-WHERE cd.facilities.facid IN (0,1) 
-AND cd.bookings.starttime >= '2012-09-21' 
-AND cd.bookings.starttime < '2012-09-22' 
-ORDER BY cd.bookings.starttime;
-
-
--- 14. Solucao:
-
-SELECT cd.bookings.starttime 
-FROM cd.bookings 
-INNER JOIN cd.members ON 
-cd.members.memid = cd.bookings.memid 
-WHERE cd.members.firstname='David' 
-AND cd.members.surname='Farrell';
-
-
- 
+CREATE TABLE conta_trabalho(
+usuario_id INTEGER REFERENCES conta(usuario_id),
+trabalho_id INTEGER REFERENCES trabalho(trabalho_id),
+data_contrato TIMESTAMP
+)
