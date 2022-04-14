3d236ddd9936262646295ea44c93ea84f86d9853
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-14 18:25:51 -0300

    Update job.sql

--- job.sql
+++ job.sql
@@ -2,7 +2,7 @@
 DELETE FROM job
 
 
--- Deleta as rows menos a que tem Cowboy.
+-- Deleta as rows menos a que tem Cowboy. Mas se usar uma segunda vez, ele apagará todas as rows.
 DELETE FROM job
 WHERE nome_trabalho = 'Cowboy'
 RETURNING trabalho_id,trabalho_nome
