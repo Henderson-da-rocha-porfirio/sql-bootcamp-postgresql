c2d833cda8546a20865a3f2cc7af4ac054bc1a8c
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-14 18:18:07 -0300

    Update conta.sql

--- conta.sql
+++ conta.sql
@@ -6,3 +6,20 @@ SET ultimo_login = CURRENT_TIMESTAMP
 -- Reset everything without WHERE condition
 UPDATE conta
 SET ultimo_login = CURRENT_TIMESTAMP
+
+
+-- Set based on another column
+UPDATE conta
+SET ultimo_login = criado_em
+
+-- Using another table’s values (UPDATE join)
+UPDATE TableA
+SET original_col = TableB.new_col
+FROM tableB
+WHERE tableA.id = TableB.id
+
+-- Return affected rows
+UPDATE conta
+SET ultimo_login = criado_em
+RETURNING conta_id,ultimo_login
+    
