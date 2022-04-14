5acfc78f47e1af120d8d54c493535f65adb90df2
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-04-14 17:26:47 -0300

    Update and rename conta.sql to create.sql

---
+++ create.sql
@@ -0,0 +1,22 @@
+CREATE TABLE conta(
+usuario_id SERIAL PRIMARY KEY,
+nomeusuario VARCHAR(50) UNIQUE NOT NULL,
+password VARCHAR(50) NOT NULL,
+email VARCHAR(250) UNIQUE NOT NULL,
+criado_em TIMESTAMP NOT NULL,
+ultimo_login TIMESTAMP
+)
+
+CREATE TABLE trabalho(
+trabalho_id SERIAL PRIMARY KEY,
+trabalho_nome VARCHAR(200) UNIQUE NOT NULL,
+)
+
+CREATE TABLE conta_trabalho(
+usuario_id INTEGER REFERENCES conta(usuario_id),
+trabalho_id INTEGER REFERENCES trabalho(trabalho_id),
+data_contrato TIMESTAMP
+)
+
+
+ 
